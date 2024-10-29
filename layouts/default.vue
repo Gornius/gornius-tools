e93c6d4d7125f981cc3737042385260599dde9c3
gornius <rafalcisek121@gmail.com>
2024-10-29 18:59:41 +0100

    countdown module

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="flex flex-col h-full min-h-dvh">
     <div class="flex flex-row gap-4 border-b border-b-border p-4">
-      <div>Logo</div>
+      <NuxtLink to="/"><div>Logo</div></NuxtLink>
       <div class="flex-grow"></div>
       <div>NavRight</div>
     </div>
     <div class="flex-grow flex flex-row divide-x border-border">
-      <div class="flex-grow max-w-sm p-4">SideNav</div>
+      <div class="flex-grow max-w-sm p-4">
+        <NuxtLink active-class="font-bold" to="/countdown">Countdown</NuxtLink>
+      </div>
       <div class="flex-grow p-4">
         <main class="h-full">
           <slot />
