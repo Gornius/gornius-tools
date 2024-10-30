934eccf396dd46a3357efcf85fb461c48d63a5b2
gornius <rafalcisek121@gmail.com>
2024-10-30 16:48:47 +0100

    layout improvements

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,25 @@
+<script setup lang="ts">
+const windowSize = useWindowSize();
+const smallMode = computed(() => windowSize.width.value <= 800);
+
+const menuClosed = ref<boolean>(false);
+const menuHidden = computed(() => !menuClosed.value && smallMode.value);
+</script>
 <template>
   <div class="flex flex-col h-full min-h-dvh">
     <div class="flex flex-row gap-4 border-b border-b-border p-4">
-      <NuxtLink to="/"><div>Logo</div></NuxtLink>
+      <div
+        :hidden="!smallMode"
+        class="cursor-pointer"
+        @click="menuClosed = !menuClosed"
+      >
+        🍔
+      </div>
+      <NuxtLink to="/"><div>Gornius Toolbox</div></NuxtLink>
       <div class="flex-grow"></div>
-      <div>NavRight</div>
     </div>
     <div class="flex-grow flex flex-row divide-x border-border">
-      <div class="flex-grow max-w-sm p-4">
+      <div class="flex-grow max-w-sm p-4" :hidden="menuHidden">
         <NuxtLink active-class="font-bold" to="/countdown">Countdown</NuxtLink>
       </div>
       <div class="flex-grow p-4">
@@ -17,3 +30,9 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+.menu-closed {
+  transform: translateX(-100%);
+}
+</style>
